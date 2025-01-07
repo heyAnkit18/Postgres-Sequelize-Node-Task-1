949f5de9755c7b5efdcf96d8469d8c47f956855d
heyAnkit18 <ankitkumarjune18@gmail.com>
2025-01-07 14:55:59 +0530

    Define user model

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const {DataTypes}=require('sequelize');
+const sequelize=require('../config');
+
+const User=sequelize.define('User',{
+    name:{
+        type: DataTypes.STRING,
+        allowNull:false
+    },
+    email:{
+        type: DataTypes.STRING,
+        allowNull:false
+    },
+    age:{
+        type: DataTypes.INTEGER,
+    },
+
+});
+module.exports=User;
